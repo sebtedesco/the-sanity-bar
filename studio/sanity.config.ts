42a9d58cdfdeefc5bc481a07971619f3f3a1946b
Sebastian Tedesco <stedescoc@gmail.com>
2023-06-24 18:35:35 -0600

    add project id and dataset to sanity config file

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,16 +1,16 @@
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from './schemas'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
 
 export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
 
 export default defineConfig({
-  name: 'project-name',
-  title: 'Project Name',
-  projectId,
-  dataset,
+  name: 'the-sanity-bar',
+  title: 'The Sanity Bar',
+  projectId: 'jata94cz',
+  dataset: 'production',
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
